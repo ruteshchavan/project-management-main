d9b86011eca9f11c96eebba99f05e6aff0e87131
ruteshchavan <ruteshrc007@gmail.com>
2025-12-13 21:30:35 +0530

    update

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
+import workspaceRouter from "./routes/workspaceRoutes.js";
+import { protect } from "./middlewares/authMiddleware.js";
 
 const app = express();
 
@@ -16,6 +18,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+
+// routes
+app.use("/api/workspaces", protect, workspaceRouter);
 
 const PORT = process.env.PORT || 5000; // if in env var. port is available it will run on it or start on 5000
 
